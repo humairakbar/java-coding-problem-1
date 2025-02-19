b5fd6a4e7521a463dbe3d5bfa631f00c423263a6
humairakbar <humairabinteakbar1234@gmail.com>
2025-02-20 00:02:49 +0600

    added sixth problem

---
+++ date.js
@@ -0,0 +1,10 @@
+const today = new Date();
+const date = new Date('2026-10-09');
+console.log(date);
+console.log(date.getDate());
+console.log(date.getMonth());
+const specifitDate = new Date(2091, 0, 26);
+console.log(specifitDate);
+specifitDate.setMonth(10);
+console.log(specifitDate);
+console.log(specifitDate.toLocaleString('en-GB'));
