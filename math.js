7c77613fe41516e9d580a8d9f3b410b2f5754365
humairakbar <humairabinteakbar1234@gmail.com>
2025-02-19 23:14:39 +0600

    added fifth problem

---
+++ math.js
@@ -0,0 +1,21 @@
+// Find min
+
+const min = Math.min(1,2,3,4,5,6,7,8,9);
+console.log(min);
+
+// Find max
+
+const max = Math.max(1,2,3,4,5,6,7,8,9);
+console.log(max);
+
+// Some other Math function
+
+console.log(Math.PI);
+console.log(Math.abs(-44));
+console.log(Math.round(2.34));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.74));
+console.log(Math.ceil(2.00001));
+
+console.log(Math.random());
+console.log(Math.random()*10);
